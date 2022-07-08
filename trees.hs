62d30b41f7d35ffe2796ac9bf350497d20a9c3cc
Oliver <34473476+aegonwolf@users.noreply.github.com>
2022-07-08 13:51:09 +0200

    tree reference

---
+++ trees.hs
@@ -0,0 +1,14 @@
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float  
+
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a  
+singleton x = Node x EmptyTree EmptyTree  
+  
+treeInsert :: (Ord a) => a -> Tree a -> Tree a  
+treeInsert x EmptyTree = singleton x  
+treeInsert x (Node a left right)   
+    | x == a = Node x left right  
+    | x < a  = Node a (treeInsert x left) right  
+    | x > a  = Node a left (treeInsert x right)  
